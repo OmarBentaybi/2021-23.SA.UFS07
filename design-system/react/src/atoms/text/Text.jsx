beab651c7e6437dbaff61d14cd73262b41b8664d
Omar <o.bentayb@itsrizzoli.it>
2022-06-14 12:23:24 +0200

    feat: add text

---
+++ design-system/react/src/atoms/text/Text.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const  Text = ({})=>{
+    return (
+        <span>Ciao Mondo</span>
+    )
+};
+Text.PropTypes={
+
+};
+
+Text.defaultProps ={
+    
+};
